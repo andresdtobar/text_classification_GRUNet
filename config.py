dae2b6d34372b61aec31f8c715e2d1280d4ff736
Andrés Tobar <34287950+andresdtobar@users.noreply.github.com>
2022-02-01 15:19:07 -0500

    add configuration file with constants and db parameters

---
+++ config.py
@@ -0,0 +1,14 @@
+# Database connection parameters
+_SERVER = '52.224.6.206' 
+_DATABASE = 'Previsora_SECOP' 
+_USERNAME = 'admin_usersql' 
+_PASSWORD = 'admin_usersql'
+
+# Database 
+_SCHEMA = 'dbo'
+_TRAINING_TABLE = 'EntrenamientoSECOP'
+_OPT_TABLE = 'DatosSECOPI'
+
+#Model constants
+_MIN_TEXT_LENGHT = 20
+_MAX_TEXT_LENGHT = 150
